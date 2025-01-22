edc12d9be4e8983c59c34e2f86bdaf4ac8a7bbe9
Yaahhyaa <yahya.shekh@student.htldornbirn.at>
2025-01-22 12:54:10 +0100

    PeopleContainer

---
+++ firstapp/src/components/PeopleContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function PeopleContainer() {
+    <div>
+        <h1>People</h1>
+        <div>
+            <div>Person1</div>
+            <div>Person2</div>
+            <div>Person3</div>
+            <div>Person4</div>
+            <div>Person5</div>
+        </div>
+    </div>
+}
